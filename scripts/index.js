839fdeb293a6a87c3e1bee4af26ff69d72182796
Jennika OBrien <jennika.obrien@gmail.com>
2023-04-15 11:04:05 -0600

    sprint 4 stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,16 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
   },
 ];
+
+/* ELEMENTS */
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileModalClose = document.querySelector("#profile-modal-close");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+profileModalClose.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
