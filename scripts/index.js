d29097e85130d4dc0c0b41f8fa98518e35a5ef61
Jennika OBrien <jennika.obrien@gmail.com>
2023-05-11 16:52:27 -0600

    task 5/7 delete button functionality

--- scripts/index.js
+++ scripts/index.js
@@ -88,6 +88,11 @@ function getCardElement(data) {
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
   const likeButton = cardElement.querySelector(".card__like-button");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
 
   likeButton.addEventListener("click", () => {
     likeButton.classList.toggle("card__like-button_active");
